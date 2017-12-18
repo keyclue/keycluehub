0863a1f26eaa54243b9c1d9270a93b08dd0ee481
Tester <testing7738@gmail.com>
2017-12-18 19:24:11 +0530

    Dewmo

--- index.js
+++ index.js
@@ -64,11 +64,11 @@ app.post('/create_collection',function(request,response){
 		if(err){
 			response.send({"error":err});  
         }
-		console.log("res"+JSON.stringify(result));
+		// console.log("res"+JSON.stringify(result));
         if(result == null) {
-			response.redirect('/sheet/'+result._id);  
+			response.redirect('/sheet/'+result["ops"]._id);  
         }else{
-			response.redirect('/sheet/'+result._id);  
+			response.redirect('/sheet/'+result["ops"]._id);  
         }
     });
 });
