7468d7294a97b6c57fbed1fa05bc2d0b9b71ac80
Tester <testing7738@gmail.com>
2017-12-20 10:42:30 +0530

    Demo

--- index.js
+++ index.js
@@ -291,12 +291,12 @@ app.all('/edit_sheet/:col_id/:brand_id', Auth, function(request, response) {
 	}else{
 		googleSheet.getSheetData( request.params,function (err, result) {
 		if(err){
-				response.render('pages/sheet',{url:"edit_sheet", title:'get',data:"", data_base:col_id, brand_id:brand_id })	
+				response.render('pages/edit_sheet',{url:"edit_sheet", title:'get',data:"", data_base:col_id, brand_id:brand_id })	
 			}
 			if(result == null) {
-				response.render('pages/sheet',{url:"edit_sheet", title:'get',data:"", data_base:col_id, brand_id:brand_id })	
+				response.render('pages/edit_sheet',{url:"edit_sheet", title:'get',data:"", data_base:col_id, brand_id:brand_id })	
 			}else{
-				response.render('pages/sheet',{url:"edit_sheet", title:'post',data:result, row:5, data_base:col_id, brand_id:brand_id})	
+				response.render('pages/edit_sheet',{url:"edit_sheet", title:'post',data:result, row:5, data_base:col_id, brand_id:brand_id})	
 			}	
 		});
 	}
